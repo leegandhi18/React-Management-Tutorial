807d2520bdf4cdb1bd7438afa39b560a569a71d4
leegandhi18 <leegandhi18@gmail.com>
2020-12-25 18:12:57 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -4,10 +4,37 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    res.send(
+        [
+            {
+              'id': 1,
+              'image': 'https://placeimg.com/64/64/1',
+              'name': '홍길동',
+              'birthday': '961222',
+              'gender': '남자',
+              'job': "대학생"
+            },
+            {
+              'id': 2,
+              'image': 'https://placeimg.com/64/64/2',
+              'name': '홍길동2',
+              'birthday': '961222',
+              'gender': '남자',
+              'job': "대학생2"
+            },
+            {
+              'id': 3,
+              'image': 'https://placeimg.com/64/64/3',
+              'name': '홍길동3',
+              'birthday': '961222',
+              'gender': '남자',
+              'job': "대학생3"
+            }
+          ]
+    );
 });
-
+ 
 app.listen(port, () => console.log(`Listening on port ${port}`));
